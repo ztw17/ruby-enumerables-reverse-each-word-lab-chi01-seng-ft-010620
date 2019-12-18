825808c32315bf4713e0ef241c2f08e40082ca70
Zach Weber <weberzt@gmail.com>
2019-12-18 23:40:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
-  reversed_words = []
-  string.split("").each do |word|
-    reversed_words <<< word.reversed
-  end
-  reversed_words.join("")
-end
+   reversed_words = []
+   string.split(" ").each do |word|
+     reversed_words << word.reverse
+   end
+   reversed_words.join(" ")
+ end
 
   
